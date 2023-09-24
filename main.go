e4425c516e8bf31c4824de1b3ad676323f6c3350
Gan (Tony) YUAN <greybluesea@gmail.com>
2023-09-24 20:36:50 +1300

    functions extracted

--- main.go
+++ main.go
@@ -1,34 +1,122 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
-func main(){
-	const appName = "GoBooking"
-	var eventName = "Go Conference"
-	const ticketsTotal uint8 = 50
-	var ticketsSold uint8 = 0
-	var ticketsRemaining uint8 = ticketsTotal - ticketsSold
+// package level variables
+const appName = "GoBooking"
+const eventName = "Go Conference"
+const ticketsTotal uint8 = 50
 
-	fmt.Println("Welcome to our booking app", appName)
+var ticketsSold uint8 = 0
+var ticketsRemaining uint8
+var bookings []string
+
+func main() {
+
+	greet()
+
+	for {
+		// variables for each customer
+		var firstName string
+		lastName := ""
+		email := ""
+		var ticketsBought uint8
+
+		// interaction for user input
+		for {
+			fmt.Print("Would you please tell me your first name:")
+			fmt.Scan(&firstName)
+
+			if len(firstName) >= 2 {
+				break
+			} else {
+				fmt.Println("Please enter a valid name that is no less than 2 characters")
+				continue
+			}
+		}
+
+		for {
+			fmt.Print("And your last name:")
+			fmt.Scan(&lastName)
+
+			if len(lastName) >= 2 {
+				break
+			} else {
+				fmt.Println("Please enter a valid name that is no less than 2 characters")
+				continue
+			}
+		}
+
+		for {
+			fmt.Print("What's your email address:")
+			fmt.Scan(&email)
+
+			if strings.Contains(email, "@") {
+				break
+			} else {
+				fmt.Println("Please re-enter a valid email address that contains '@'")
+				continue
+			}
+		}
+
+		for {
+			fmt.Print("How many tickets do you want to buy:")
+			fmt.Scan(&ticketsBought)
+			if ticketsBought <= 0 {
+				fmt.Println("The number of tickets you want to buy has to be a positive number")
+				continue
+			} else if ticketsBought > ticketsRemaining {
+				fmt.Printf("we have only %d tickets left, please re-enter\n", ticketsRemaining)
+				continue
+			} else {
+				break
+			}
+		}
+
+		// confirmation of user input
+		fmt.Printf("Thank you %s %s for booking %d tickets. Tickets will be sent to your email address: %s\n", firstName, lastName, ticketsBought, email)
+		bookings = append(bookings, firstName)
+
+		// count tickets
+		countTickets(ticketsBought)
+
+		// count bookings
+		countBookings(firstName)
+
+		// check for ending
+		if ticketsRemaining == 0 {
+			fmt.Printf("Tickets for %s have been all sold out. Come back next year! \n", eventName)
+			break
+		}
+	}
+}
+
+func greet() {
+	fmt.Printf("\tWelcome to our booking app: %s!\n", appName)
 	fmt.Printf("Get your ticket here for the event: %s\n", eventName)
-	fmt.Printf("we have %d of %d tickets available\n",ticketsRemaining, ticketsTotal )
+	fmt.Printf("we have %d tickets in total available\n", ticketsTotal)
+}
 
-	var firstName string
-	var lastName string
-	var email string
-	var ticketsBought uint8
-  
-	fmt.Println("Would you please tell me your first name:")
-	fmt.Scan(&firstName)
-	fmt.Println("And your last name:")
-	fmt.Scan(&lastName)
-	fmt.Println("What's your email address:")
-	fmt.Scan(&email)
-	fmt.Println("How many tickets do you want to buy:")
-	fmt.Scan(&ticketsBought) 
-	ticketsSold = ticketsBought
+func useFirstNameBookings(bookings []string) []string {
+	var firstNameBookings = []string{}
+	for _, booking := range bookings {
+		var fullName = strings.Fields(booking)
+		firstNameBookings = append(firstNameBookings, fullName[0])
+	}
+	return firstNameBookings
+}
+
+func countTickets(ticketsBought uint8) {
+	ticketsSold = ticketsSold + ticketsBought
 	ticketsRemaining = ticketsTotal - ticketsSold
-   
-	fmt.Printf("Thank you %s %s for booking %d tickets. Tickets will be sent to your email address: %s\n", firstName,lastName, ticketsBought, email)
-	fmt.Printf("We now have %d tickets remaining for %s\n", ticketsRemaining,eventName)
+	fmt.Printf("We now have %d tickets remaining for %s.\n", ticketsRemaining, eventName)
+}
+
+func countBookings(firstName string) {
+	bookings = append(bookings, firstName)
+	firstNameBookings := useFirstNameBookings(bookings)
+	fmt.Printf("Our current booking customers are: %v \n", firstNameBookings)
 }
